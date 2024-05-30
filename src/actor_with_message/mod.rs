f2a1bb961778cd43f51acef8c68971883ca95326
Tong Nguyen <tong@acuity.sg>
2024-05-30 19:27:31 +0700

    some simple actor instance

---
+++ src/actor_with_message/mod.rs
@@ -0,0 +1,50 @@
+use async_trait::async_trait;
+use tokio::sync::mpsc;
+
+use crate::actor::{Actor, ActorHandler};
+
+#[derive(Debug)]
+pub enum ActorMessage {
+    Get { value: u32 },
+}
+
+pub struct ActorWithMessage {
+    pub name: String,
+    receiver: mpsc::Receiver<ActorMessage>,
+}
+
+#[async_trait]
+impl Actor for ActorWithMessage {
+    async fn run(&mut self) {
+        while let Some(msg) = self.receiver.recv().await {
+            self.handle_message(msg);
+        }
+    }
+}
+
+impl ActorWithMessage {
+    pub fn new(name: String, receiver: mpsc::Receiver<ActorMessage>) -> Self {
+        Self { name, receiver }
+    }
+
+    fn handle_message(&self, msg: ActorMessage) {
+        println!("{} received message {:?}", self.name, msg);
+    }
+}
+
+#[derive(Clone)]
+pub struct ActorWithMessageHandler {
+    sender: mpsc::Sender<ActorMessage>,
+}
+
+impl ActorHandler for ActorWithMessageHandler {}
+
+impl ActorWithMessageHandler {
+    pub fn new(sender: mpsc::Sender<ActorMessage>) -> Self {
+        Self { sender }
+    }
+
+    pub async fn send_message(&self, msg: ActorMessage) {
+        self.sender.send(msg).await.unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
 pub mod actor;
+pub mod actor_with_message;
 pub mod simple_actor;
 
 use actor::ActorHandler;
-use simple_actor::SimpleActor;
+use actor_with_message::{ActorMessage, ActorWithMessage, ActorWithMessageHandler};
+use simple_actor::{SimpleActor, SimpleActorHandler};
 
 #[tokio::main]
 async fn main() {
+    // Example #1: A very simple actor with an infinite loop that increments a state variable
     let simple_actor = SimpleActor::new("SimpleActor".to_string());
-    let simple_actor_handler = simple_actor::SimpleActorHandler::new();
+    let simple_actor_handler = SimpleActorHandler::new();
     simple_actor_handler.start(simple_actor);
 
+    // Example 2: An actor with an infinite loop that listen for messages
+    let (sender, receiver) = tokio::sync::mpsc::channel(100);
+    let actor_message = ActorWithMessage::new("ActorWithMessage".to_string(), receiver);
+    let actor_message_handler = ActorWithMessageHandler::new(sender);
+    actor_message_handler.start(actor_message);
+    start_seding_to_actor_message(&actor_message_handler);
+
     loop {}
+}
+
+fn start_seding_to_actor_message(handler: &ActorWithMessageHandler) {
+    let handler = handler.clone();
+    tokio::spawn(async move {
+        let mut counter = 0;
+        loop {
+            handler
+                .send_message(ActorMessage::Get { value: counter })
+                .await;
+            counter += 1;
+            tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+        }
+    });
 }
